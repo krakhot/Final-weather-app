c4ded1d0bb80e5ea3dfcf79e5a5d1c41cfb75acb
krakhot <64920031+krakhot@users.noreply.github.com>
2022-06-09 14:45:58 +0200

    Added current location feature & link to github source code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let apiKey = "6012fc2491a7112eae2e7a250ec9ffa1";
+
 function formatDate() {
   let now = new Date();
   let weekDays = [
@@ -20,15 +22,29 @@ function formatDate() {
   }
   return `${day} ${hours}:${minutes}`;
 }
+function determineCurrentCity(response){
+  apiSearch(response.data[0].name)
+}
+function determineCoordinates(position) {
+  let lat = position.coords.latitude
+  let lon = position.coords.longitude
+  let geoLocUrl = `http://api.openweathermap.org/geo/1.0/reverse`
+  let apiUrl = `${geoLocUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}`
+  axios.get(apiUrl).then(determineCurrentCity)
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault()
+  navigator.geolocation.getCurrentPosition(determineCoordinates)
+}
 function handleSubmit(event) {
   event.preventDefault();
   let userCity = document.querySelector("#city-input").value;
-  search(userCity);
+  apiSearch(userCity);
 }
 
-function search(city) {
+function apiSearch(city) {
   let weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
-  let apiKey = "6012fc2491a7112eae2e7a250ec9ffa1";
   let unit = "metric";
   let apiUrl = `${weatherUrl}?q=${city}&units=${unit}&appid=${apiKey}`;
   axios.get(apiUrl).then(updateWeatherData);
@@ -57,4 +73,8 @@ function updateWeatherData(response) {
 document
   .querySelector("#search-city-form")
   .addEventListener("submit", handleSubmit);
-search("Annecy");
+document.querySelector("#current-position-button")
+  .addEventListener("click", getCurrentLocation)
+
+
+apiSearch("Annecy");
