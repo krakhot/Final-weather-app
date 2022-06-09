dc46f05ed6cad3bbfee333da5125bb1813b83f2e
krakhot <64920031+krakhot@users.noreply.github.com>
2022-06-09 18:59:41 +0200

    Added unit conversion
    "active" class to use the css properties and make links appear like links or not

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 let apiKey = "6012fc2491a7112eae2e7a250ec9ffa1";
+let tempToday = document.querySelector("#temperature-today")
+let celsiusTemperature = null
+let celsiusLink = document.querySelector("#celsius-link")
+let fahrenheitLink = document.querySelector("#fahrenheit-link")
 
+function apiSearch(city) {
+  let weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
+  let unit = "metric";
+  let apiUrl = `${weatherUrl}?q=${city}&units=${unit}&appid=${apiKey}`;
+  axios.get(apiUrl).then(updateWeatherData);
+}
+function convertToCelsius(event){
+  event.preventDefault()
+  tempToday.innerHTML = Math.round(celsiusTemperature)
+  celsiusLink.classList.add("active")
+  fahrenheitLink.classList.remove("active")
+}
+function convertToFahrenheit(event){
+  event.preventDefault()
+  let fahrenheitTemp = (celsiusTemperature * 9) / 5 + 32
+  tempToday.innerHTML = Math.round(fahrenheitTemp)
+  celsiusLink.classList.remove("active")
+  fahrenheitLink.classList.add("active")
+}
+function determineCurrentCity(response){
+  apiSearch(response.data[0].name)
+}
+function determineCoordinates(position) {
+  let lat = position.coords.latitude
+  let lon = position.coords.longitude
+  let geoLocUrl = `http://api.openweathermap.org/geo/1.0/reverse`
+  let apiUrl = `${geoLocUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}`
+  axios.get(apiUrl).then(determineCurrentCity)
+}
 function formatDate() {
   let now = new Date();
   let weekDays = [
@@ -22,17 +55,6 @@ function formatDate() {
   }
   return `${day} ${hours}:${minutes}`;
 }
-function determineCurrentCity(response){
-  apiSearch(response.data[0].name)
-}
-function determineCoordinates(position) {
-  let lat = position.coords.latitude
-  let lon = position.coords.longitude
-  let geoLocUrl = `http://api.openweathermap.org/geo/1.0/reverse`
-  let apiUrl = `${geoLocUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}`
-  axios.get(apiUrl).then(determineCurrentCity)
-}
-
 function getCurrentLocation(event) {
   event.preventDefault()
   navigator.geolocation.getCurrentPosition(determineCoordinates)
@@ -42,33 +64,20 @@ function handleSubmit(event) {
   let userCity = document.querySelector("#city-input").value;
   apiSearch(userCity);
 }
-
-function apiSearch(city) {
-  let weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
-  let unit = "metric";
-  let apiUrl = `${weatherUrl}?q=${city}&units=${unit}&appid=${apiKey}`;
-  axios.get(apiUrl).then(updateWeatherData);
-}
-
 function updateWeatherData(response) {
   document.querySelector("#displayed-city").innerHTML = response.data.name;
-  document.querySelector("#temperature-today").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  celsiusTemperature = response.data.main.temp
+  tempToday.innerHTML = Math.round(celsiusTemperature);
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
+  document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#date").innerHTML = formatDate();
   let weatherIcon = response.data.weather[0].icon;
   let icon = document.querySelector("#weather-icon");
-  icon.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
-  );
+  icon.setAttribute("src",`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`);
   icon.setAttribute("alt", response.data.weather[0].description);
+
 }
 document
   .querySelector("#search-city-form")
@@ -76,5 +85,12 @@ document
 document.querySelector("#current-position-button")
   .addEventListener("click", getCurrentLocation)
 
-
 apiSearch("Annecy");
+
+
+//unit conversion
+
+
+
+celsiusLink.addEventListener("click", convertToCelsius)
+fahrenheitLink.addEventListener("click", convertToFahrenheit)
